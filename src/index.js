16643ea033bf739fb9a4d44b4de8530d8cc10b82
eustaquio <eustaquio.galano@gmail.com>
2025-03-08 18:00:12 +0800

    refactor: reset selected files for project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import { greetings } from "./greetings.js";
-import "./styles.css";
-import ubeng from "./img/ubeng.png"
 
-console.log(greetings);
-
-const rob = document.createElement("img");
-rob.src = ubeng;
-
-document.body.appendChild(rob);
