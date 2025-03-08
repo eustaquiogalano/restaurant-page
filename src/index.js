1156850bf4d14a56989a7cf53c1e82b672581432
eustaquio <eustaquio.galano@gmail.com>
2025-03-08 12:50:35 +0800

    feat: add image using javascript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import { greetings } from "./greetings.js";
 import "./styles.css";
+import ubeng from "./img/ubeng.png"
 
 console.log(greetings);
+
+const rob = document.createElement("img");
+rob.src = ubeng;
+
+document.body.appendChild(rob);
