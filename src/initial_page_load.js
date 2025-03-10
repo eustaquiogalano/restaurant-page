cb072c1c58374da2b7e369f6742217eb85edb128
eustaquio <eustaquio.galano@gmail.com>
2025-03-11 00:10:28 +0800

    chore: add comments

--- src/initial_page_load.js
+++ src/initial_page_load.js
@@ -52,16 +52,21 @@ import menuImageImport from './img/menu.png';
     const restoMenu = document.createElement('div');
     restoMenu.id = 'resto-menu';
 
+    // create an h3 element for the title of the div
     const menu = document.createElement('h3');
     menu.textContent = 'Menu';
 
+    // append to the second div
     restoMenu.appendChild(menu);
 
+    // create an image element and assign its src and alt attribute
     const menuImage = document.createElement('img');
     menuImage.src = menuImageImport;
     menuImage.alt = 'restaurant menu';
 
+    // append to the second div
     restoMenu.appendChild(menuImage);
 
+    // append the second div (resto-menu) to the content div
     content.appendChild(restoMenu);
 })();
