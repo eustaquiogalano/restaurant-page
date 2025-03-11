12520af12dfd9d21846f389411211ecc35e47bc2
eustaquio <eustaquio.galano@gmail.com>
2025-03-12 01:18:01 +0800

    feat: initiate tab browsing functionality
    - update default imports to named imports
    - invoke home module as initial page
    - get a reference to the 3 buttons to be used for tab browsing
    - put an event listener to home and menu buttons (test phase)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-import "./styles.css"
-import "./initial_page_load";
+import "./styles.css";
+import {homeModule} from "./initial_page_load.js";
+import {menuModule} from "./menu_module.js";
+
+homeModule();
+const homeButton = document.querySelector('#home');
+const menuButton = document.querySelector('#menu');
+const aboutButton = document.querySelector('#about');
+
+const content = document.querySelector('#content');
+
+homeButton.addEventListener('click', () => {
+    content.innerHTML = '';
+    homeModule();
+});
+menuButton.addEventListener('click', () => {
+    content.innerHTML = '';
+    menuModule();
+});
