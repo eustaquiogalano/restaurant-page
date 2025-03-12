41c3ecb97bf12e1032e373c04f9713a8eee446ee
eustaquio <eustaquio.galano@gmail.com>
2025-03-13 01:11:09 +0800

    feat: set black background color
    When a tab is clicked, set its background color to black to indicate
    selection.

--- src/index.js
+++ src/index.js
@@ -13,13 +13,22 @@ const content = document.querySelector('#content');
 homeButton.addEventListener('click', () => {
     content.innerHTML = '';
     homeModule();
+    homeButton.style.backgroundColor = "black";
+    menuButton.style.backgroundColor = "white";
+    aboutButton.style.backgroundColor = "white";   
 });
 menuButton.addEventListener('click', () => {
     content.innerHTML = '';
     menuModule();
+    homeButton.style.backgroundColor = "white";
+    menuButton.style.backgroundColor = "black";
+    aboutButton.style.backgroundColor = "white";   
 });
 
 aboutButton.addEventListener('click', () => {
     content.innerHTML = '';
     aboutModule();
+    homeButton.style.backgroundColor = "white";
+    menuButton.style.backgroundColor = "white";
+    aboutButton.style.backgroundColor = "black";   
 });
